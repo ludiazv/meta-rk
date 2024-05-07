c7b61465ed1572ef4e3162e28e8305ca63c3bac9
ludiazv <ldiaz@atloideas.com>
2024-05-07 10:38:43 +0200

    upgrade uwe5622 patches

--- recipes-kernel/linux/linux-yocto/rockchip-kmeta/bsp/rk35/uwe5622/download_patches.sh
+++ recipes-kernel/linux/linux-yocto/rockchip-kmeta/bsp/rk35/uwe5622/download_patches.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Simple scrit to downlload uwe patches from armbian using 
+# build/lib/functions/compilation/patch/drivers_network.sh
+# function driver_uwe5622()
 
-PREFIX_MISC="https://raw.githubusercontent.com/armbian/build/main/patch/misc/"
+
+PREFIX_MISC="https://raw.githubusercontent.com/armbian/build/main/patch/misc/wireless-uwe5622/"
 PREFIX_RK="https://raw.githubusercontent.com/armbian/build/main/patch/kernel/archive/rockchip64-6.6/"
 
 if [ $# -lt 1 ] ; then
@@ -12,10 +16,18 @@ while read e ; do
 
  f=$(echo $e | cut -f 2 -d ' ')
 
+ if echo $f | grep '000' > /dev/null ; then
+    echo "Ignore $f"
+    continue
+ fi
+
  url="${PREFIX_RK}${f}"
  if echo $f | grep 'wireless-' >/dev/null ; then
    url="${PREFIX_MISC}${f}"
+ elif echo $f | grep 'uwe5622-' >/dev/null; then
+   url="${PREFIX_MISC}${f}"
  fi
+ 
  echo "Downloading $f [$url] ..."
  wget "${url}" -O "./$f"
 
